4daf894be39b7473bb2001e7d5950fe94059f904
Maria-nixi <mariamarcenko731@gmail.com>
2025-11-28 16:45:29 +0000

    тут есть функции и хорошая инфа

---
+++ lec_doc.py
@@ -0,0 +1,12 @@
+def crutoi_chuvak(a=1, b=1, c=1):
+    '''
+    Крайне мотивирующая функция!
+    '''
+    a = 'Pofic'
+    b = 'Pofic'
+    
+    print('Я молодец!')
+
+crutoi_chuvak()
+
+help(print)
