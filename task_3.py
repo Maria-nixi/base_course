be2ef056d1a96ab27b68adef5657279c4ed49dac
Maria-nixi <mariamarcenko731@gmail.com>
2026-01-16 14:32:24 +0000

    немного доделала

--- task_3.py
+++ task_3.py
@@ -2,13 +2,7 @@ year = int(input())
 a = year % 4 == 0
 b = year % 100 == 0
 c = year % 400 == 0
-if a:
-    if b:
-        print(f'{year} - високостный')
-        
-elif b and c:
-    print(f'{year} - високостный')
-elif c:
+if a and (b and c):
     print(f'{year} - високостный')
 else:
     print(f'{year} - не високостный')
